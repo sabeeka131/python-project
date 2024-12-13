ad568201f57dd3d7ea67537fd632994c7ae7d80a
Aliza <59551300+sabeeka131@users.noreply.github.com>
2024-12-13 18:58:59 +0500

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,6 @@
+temperature = int(input("Please enter the current temperature in Celsius: "))
+limit = 20.0 
+if (temperature > 20):
+ print("The temperature is", temperature, "It's too cold for light clothes. Wear something warmer.")
+else:
+  print("The temperature is", temperature, "It's suitable for wearing light clothes.")
